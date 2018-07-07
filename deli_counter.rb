3c9cce9b4d68268761ccc8a7e89cfa83f43a8c01
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-07 10:34:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ end
 
 
 def take_a_number(katz_deli, name = "Ada")
-  puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
+  puts "Welcome, #{name.map}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
