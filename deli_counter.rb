39040ec777baa9669c6d0e7944e92d0824cc7e4d
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-07 10:25:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,9 +20,9 @@ def take_a_number(katz_deli, name = "Ada")
 end
 
 def now_serving(katz_deli)
-  if katz_deli == 0
-    puts "There is nobody waiting to be served!"
+  if katz_deli >= 1
+    puts "Currently serving #{katz_deli.shift}."
   else
-  puts "Currently serving #{katz_deli.shift}."
+    puts "There is nobody waiting to be served!"
 end
 end
