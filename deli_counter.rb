0dc76a0be0222d43e3a17088f0955a494796968e
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-07 10:09:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else 
+    puts "The line is currently: #{katz_deli.join(", ")}."
+  end
+end
+
+
+# primary_colors.each do |color|
+# puts "Primary Color #{color} is #{color.length} letters long."
+# end
+
+
+def take_a_number(katz_deli, name = "Ada")
+  puts "Welcome, #{name}. You are number #{katz_deli} in line."
+end
+
+def now_serving(katz_deli)
+  puts "Currently serving #{katz_deli}"
+end
