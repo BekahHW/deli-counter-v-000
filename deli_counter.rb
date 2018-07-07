3246ebf2e6ff47800f3db72ddcac8267c71637c4
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-07 11:13:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ number_in_line= ("#{katz_deli.length} + 1")
 
 
 def line(katz_deli)
-  katz_deli_with_num = katz_deli.each_with_index.map { |x,i| [(i+1, ". ", x)]}
+  katz_deli_with_num = katz_deli.each_with_index.map { |x,i| [i+1, ". ", x]}
   if katz_deli.length == 0
     puts "The line is currently empty."
   else 
