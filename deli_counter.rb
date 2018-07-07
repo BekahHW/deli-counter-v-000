9f889ea273b81955ec70fc7244459f95c5779d85
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-07-07 11:45:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else 
     puts "The line is currently: 
-    #{katz_deli_with_num.join(", ")}."
+    #{katz_deli_with_num.join("")}."
   end
 end
 
